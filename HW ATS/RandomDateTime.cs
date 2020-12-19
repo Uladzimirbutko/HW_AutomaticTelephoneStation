4889d92302757de267bfa51847dc8806a7c81f17
Uladzimirbutko <73130124+Uladzimirbutko@users.noreply.github.com>
2020-12-20 00:38:05 +0300

    V1.1

--- HW ATS/RandomDateTime.cs
+++ HW ATS/RandomDateTime.cs
@@ -7,10 +7,10 @@ namespace HW_ATS
             private static DateTime start = new DateTime(2020, 11, 19);
             private static int range = (DateTime.Today - start).Days;
 
-        public static DateTime Next()
+            public static DateTime Next()
             {
                 return start.AddDays(new Random().Next(range))
-                    .AddHours(new Random().Next(1,60))
+                    .AddHours(new Random().Next(1,24))
                     .AddMinutes(new Random().Next(0, 60))
                     .AddSeconds(new Random().Next(0, 60));
             }
